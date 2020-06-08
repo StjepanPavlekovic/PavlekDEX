5cfdff8b21369d114d0a5dae8008dfdb7b817b81
Stjepan Pavleković <stjepan.pavlekovic1@gmail.com>
2020-06-08 19:44:23 +0200

    BEgin closer work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
-import Container from './components/container/container.component'
+import React from "react";
+import "./App.css";
+import Container from "./components/container/container.component";
 
 function App() {
   return (
-   <div>
-     <Container />
-   </div>
+    <div>
+      <div className="closer-right"></div>
+      <div className="closer-left">
+        <div className="spinner"></div>
+      </div>
+      <Container />
+    </div>
   );
 }
 
